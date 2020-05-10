6d6e1cf68e6471041fc80d2d087a2553d2d37071
samimran <sam.imran001@gmail.com>
2020-05-10 14:55:33 +0500

    last modify by usama at may 10 300:pm

---
+++ site/js/login.js
@@ -0,0 +1,13 @@
+// <script language="javascript">
+//   alert("Error Password or Username");
+//
+//     function check(form) { /*function to check userid & password*/
+//         /*the following code checkes whether the entered userid and password are matching*/
+//         if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd") {
+//             window.open('https://www.google.com/')/*opens the target page while Id & password matches*/
+//         }
+//         else {
+//             alert("Error Password or Username")/*displays error message*/
+//         }
+//     }
+// </script>
