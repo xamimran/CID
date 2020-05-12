3ac664046f91c8dbfb1155aa678962727f163b53
samimran <sam.imran001@gmail.com>
2020-05-12 23:55:49 +0500

    last changes 12 may 11:55 pm

--- site/js/login.js
+++ site/js/login.js
@@ -1,13 +1,12 @@
-// <script language="javascript">
-//   alert("Error Password or Username");
-//
-//     function check(form) { /*function to check userid & password*/
-//         /*the following code checkes whether the entered userid and password are matching*/
-//         if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd") {
-//             window.open('https://www.google.com/')/*opens the target page while Id & password matches*/
-//         }
-//         else {
-//             alert("Error Password or Username")/*displays error message*/
-//         }
-//     }
-// </script>
+function check() {
+  
+    if(document.getElementById('userid').value == "AliSubhani" && document.getElementById('pss').value == "sem020") {
+        window.open('Cases.html')
+    }
+    else if(document.getElementById('userid').value  == "Hammad" && document.getElementById('pss').value == "sem025") {
+        window.open('Police Managment.html')
+    }
+    else {
+        alert("Error Password or Username")
+    }
+}
